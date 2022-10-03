868c5fed99787f661b0d115a41f7d5d0db5ce166
JiwonLeePortfolio <100000jilee@gmail.com>
2022-10-03 10:37:37 -0500

    loadBalancer manages queue and sends to idle server

--- loadBalancer.cpp
+++ loadBalancer.cpp
@@ -21,22 +21,39 @@ int main() {
      cout << "Time to run: "; //how long to run the load balancer
      cin >> time;
 
+     Webserver wb(servers);
+
      //create an initial queue of server*2
      for (int i = 0; i < servers*2; i++) {
           Request r;
           requests_q.push(r);
      }
 
+     Request curr_req;
+     int idle_pos = -1;
+
      //manage the time
-     //for (int curr_time = 0; curr_time < t; curr_time++) {
+     for (int curr_time = 0; curr_time < time; curr_time++) {
 
-          // if there is empty server:
-               //pop and send request to server
+          // simulate requests added at random times
+          if (rand() % 2 > 0) {
+               Request r;
+               requests_q.push(r);
+          }
+
+          //if idle server, pop and send request to server
+          idle_pos = wb.has_idle_processor();
+          if (idle_pos != -1){
+               curr_req = requests_q.front();
+               wb.request_to_server(curr_req, idle_pos);
+               requests_q.pop();
+          }
 
 
-          // exit when time is done OR (queue is empty AND all requests complete)
-          //if( requests_q.empty() && no processor is holding a request)
-     //}
+          //exit when time is done OR (queue is empty AND all requests complete)
+          if( requests_q.empty() && wb.all_processors_empty())
+               break;
+     }
 
      
 
